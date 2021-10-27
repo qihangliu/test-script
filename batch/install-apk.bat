e6252940b5866c15c4f862ccace8a7c1e186da22
root <root@E14.localdomain>
2021-10-27 09:37:17 +0800

    add install apks

---
+++ batch/install-apk.bat
@@ -0,0 +1,15 @@
+@ECHO off  
+chcp 65001
+@REM 无限循环的标签  
+:LOOP  
+ECHO 请连接设备…… 
+adb wait-for-device  
+@REM 循环安装本目录下的APK文件  
+FOR %%i IN (*.apk) DO (   
+    ECHO 正在安装：%%i  
+    adb install "%%i"  
+)  
+ECHO 安装完毕；请更换下一台设备！
+PAUSE  
+GOTO LOOP  
+@ECHO on
